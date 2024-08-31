7d554981899a1e0874257ec3b24d7555fa6a618e
Aytan <aytanumudova@gmail.com>
2024-08-31 20:22:10 +0400

    the codes related to the delete button have been changed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,51 +1,57 @@
-const userName = document.querySelector("#username");
-const nameMessage = document.querySelector("#username-message");
+document.addEventListener("DOMContentLoaded", () => {
+  const userName = document.querySelector("#username");
+  const nameMessage = document.querySelector("#username-message");
 
-const age = document.querySelector("#age");
-const ageMessage = document.querySelector("#age-message");
+  const age = document.querySelector("#age");
+  const ageMessage = document.querySelector("#age-message");
+  const userForm = document.querySelector("#user-form");
 
-const sendBtn = document.querySelector("#sendBtn");
+  const tbody = document.querySelector("#tbody");
 
-const tbody = document.querySelector("#tbody");
+  const users = [];
+  userForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if (userName.value.trim() === "") {
+      ageMessage.innerHTML = "";
+      nameMessage.innerHTML = "Please fill in this field";
+    } else if (userName.value.length < 4) {
+      nameMessage.innerHTML = "Username must be at least 4 characters long";
+    } else if (age.value.trim() === "") {
+      nameMessage.innerHTML = "";
+      ageMessage.innerHTML = "Please fill in this field";
+    } else if (age.value < 18 || age.value > 65) {
+      nameMessage.innerHTML = "";
+      ageMessage.innerHTML = "Please enter 18-65 age";
+    } else {
+      ageMessage.innerHTML = "";
+      nameMessage.innerHTML = "";
 
-const users = [];
-sendBtn.addEventListener("click", () => {
-  if (userName.value.trim() === "") {
-    ageMessage.innerHTML = "";
-    nameMessage.innerHTML = "Please fill in this field";
-  } else if (userName.value.length < 4) {
-    nameMessage.innerHTML = "Username must be at least 4 characters long";
-  } else if (age.value.trim() === "") {
-    nameMessage.innerHTML = "";
-    ageMessage.innerHTML = "Please fill in this field";
-  } else if (age.value < 18) {
-    nameMessage.innerHTML = "";
-    ageMessage.innerHTML = "You can not be under 18";
-  } else if (age.value > 65) {
-    nameMessage.innerHTML = "";
-    ageMessage.innerHTML = "You can not be over 65";
-  } else {
-    ageMessage.innerHTML = "";
-    nameMessage.innerHTML = "";
+      const user = {
+        username: userName.value,
+        age: age.value,
+        id: 0,
+      };
+      users.push(user);
+      user.id = users.length;
 
-    const user = {
-      username: userName.value,
-      age: age.value,
-      id: 0,
-    };
-    users.push(user);
-    user.id = users.length;
-
-    tbody.innerHTML += `
-                <tr id="row">
-                <td>${users.indexOf(user) + 1}</td>
+      tbody.innerHTML += `
+                <tr>
+                <td>${user.id}</td>
                 <td>${user.username}</td>
                 <td>${user.age}</td>
-                <td><button class="btn-delete"><i class="fa-regular fa-trash-can delete-icon"></i></button></td>
+                <td><button id="delete-button"><i class="fa-regular fa-trash-can"></i></button></td>
                 </tr>
                 `;
-    userName.value = "";
-    age.value = "";
-    console.log(user);
-  }
+      userName.value = "";
+      age.value = "";
+    }
+  });
+  tbody.addEventListener("click", (e) => {
+    if (e.target.closest("#delete-button")) {
+      const row = e.target.closest("tr");
+      if (row) {
+        row.remove();
+      }
+    }
+  });
 });
